c490bc1774a63fd799076c099ee6b431b7946351
Jeisson Hidalgo <jeissonh@gmail.com>
2014-09-02 14:04:05 -0600

    ngossip: implementada la factorizacion prima en el comando ngossip

--- ngossip/main.c
+++ ngossip/main.c
@@ -6,16 +6,24 @@
 
 // Function prototypes or function signatures
 int print_help();
+int print_version();
 bool is_integer(const char* text);
 int print_properties(const char* num);
+void print_prime_factors(longnum num);
+
 
 int main(int argc, char* argv[])
 {
-	for ( int i = 0; i < argc; ++i )
-	{
-		// printf("Param %i: [%s]\n", i, argv[i]);
+	for ( int i = 1; i < argc; ++i )
 		if ( strcmp( argv[i], "--help" ) == 0 )
 			return print_help();
+
+	for ( int i = 1; i < argc; ++i )
+		if ( strcmp( argv[i], "--version" ) == 0 )
+			return print_version();
+
+	for ( int i = 1; i < argc; ++i )
+	{
 		if ( is_integer(argv[i]) )
 			print_properties(argv[i]);
 	}
@@ -26,16 +34,28 @@ int main(int argc, char* argv[])
 int print_help()
 {
 	printf("%s",
-		"Usage: ngossip [-lV] [NUMBERS] [PROPERTIES] [-f FILE]\n"
-		"Number Gossip: Show properties of positive integer NUMBERS. Options:\n"
+		   "Usage: ngossip [-iltV] [NUMBERS] [PROPERTIES] [-i FILE]\n"
+		   "Number Gossip: Show properties of positive integer NUMBERS. Options:\n"
 
-		"  -f FILE          Read numbers from FILE\n"
-		"  -l, --list       List all available properties\n"
-		"  -t, --transpose  For each PROPERTIES show applying numbers\n"
-		"      --version    Print version of this program\n"
+		   "  -i FILE          Read numbers from FILE\n"
+		   "  -l, --list       List all available properties\n"
+		   "  -t, --transpose  For each PROPERTIES show applying numbers\n"
+		   "  -v, --verbose    Show a short description of each property\n"
+		   "      --version    Print version of this program\n"
 
-		"If not parameters are given, read numbers from standard input until EOF\n"
-		"NUMBERS can have ranges in form num1:numN, e.g: 80:120\n");
+		   "If not parameters are given, read numbers from standard input until EOF\n"
+		   "NUMBERS can have ranges in form n1:n2, e.g: 80:120\n");
+
+	return 0;
+}
+
+int print_version()
+{
+	printf("%s",
+		"ngossip v1.0 [2014-Set-02] Jeisson Hidalgo-Céspedes <jeissonh@gmail.com>\n"
+		"\n"
+		"This is free software distributed under Creative Commons Attribution 4.0\n"
+		"International (CC BY 4.0) license.\n");
 
 	return 0;
 }
@@ -56,7 +76,9 @@ int print_properties(const char* text)
 	longnum num = strtoull(text, NULL, 10);
 	if ( num == 0 ) return 1;
 
-	printf("%llu:\n", num);
+	printf("%llu:\nprime factors: ", num);
+	print_prime_factors(num);
+	printf("\n");
 
 	if ( is_abundant(num) ) printf(" abundant");
 	if ( is_amicable(num) ) printf(" amicable");
@@ -120,3 +142,22 @@ int print_properties(const char* text)
 	printf("\n\n");
 	return 0;
 }
+
+
+void print_prime_factors(longnum num)
+{
+	for (longnum prime = 2; num > 1; ++prime )
+	{
+		longnum times = 0;
+		for ( ; num % prime == 0; ++times )
+			num /= prime;
+		if ( times > 0 )
+		{
+			printf("%llu", prime);
+			if ( times > 1 )
+				printf("^%llu", times);
+			printf(" ");
+		}
+	}
+}
+
